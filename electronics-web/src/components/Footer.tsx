18112dece77e2c90a4733b0dbd4ac9b4481fd36c
alimurtaza8 <alibalochbirmani760@gmail.com>
2025-08-11 21:52:37 +0500

    changed-keys

--- electronics-web/src/components/Footer.tsx
+++ electronics-web/src/components/Footer.tsx
@@ -266,7 +266,7 @@ const Footer = () => {
                           </span>
                         )}
                         {link.new && (
-                          <span className="bg-green-500 text-white text-xs px-2 py-0.5 rounded-full font-bold">
+                          <span className="bg-green-400 text-white text-xs px-2 py-0.5 rounded-full font-bold">
                             NEW
                           </span>
                         )}
